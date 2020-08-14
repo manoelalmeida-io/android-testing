ad72153afeff095fcb65cdbf99c9341456476c55
Manoel Almeida <manoel.almeida@bandtec.com.br>
2020-08-14 14:44:05 -0300

    Testing Fragment

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -3,6 +3,9 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
@@ -11,8 +14,10 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+
+	// Use a fake repository to be injected into the view model
+	private lateinit var tasksRepository: FakeTestRepository
 
 	private lateinit var tasksViewModel: TasksViewModel
 
@@ -21,7 +26,12 @@ class TasksViewModelTest {
 
 	@Before
 	fun setupViewModel() {
-		tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+		tasksRepository = FakeTestRepository()
+		val task1 = Task("Title1", "Description1")
+		val task2 = Task("Title2", "Description2")
+		val task3 = Task("Title3", "Description3")
+		tasksRepository.addTasks(task1, task2, task3)
+		tasksViewModel = TasksViewModel(tasksRepository)
 	}
 
 	@Test
